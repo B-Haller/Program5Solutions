3310e3cfab0fd36218f4f594e56c2887a98189c4
SeedPaths-User <lessdismissive@email.com>
2014-10-30 17:51:45 -0600

    GroupMaker10000

--- FlipManiaProgram5/FlipManiaProgram5/Program.cs
+++ FlipManiaProgram5/FlipManiaProgram5/Program.cs
@@ -12,19 +12,27 @@ namespace FlipManiaProgram5
         static void Main(string[] args)
         {
 
-            Flip(10);    
+            Flip(10000);
+            FlipForHeads(10000);
+    
 
             Console.ReadKey();
 
         }
+        /// <summary>
+        /// flips a coin, keep track of heads and tails
+        /// </summary>
+        /// <param name="numFlip">number of times to flip</param>
         static void Flip(int numFlip)
         {
+            //new random number and setting it to a variable
             Random rng = new Random();
             int randomNumber = rng.Next(0, 2);
 
+            //set variables to track heads and trail
             int numberOfHeads = 0;
             int numberOfTails = 0;
-
+            //loop and conditions to continue flip
             for (int i = 0; i <= numFlip; i++)
             {
                 randomNumber = rng.Next(0, 2);
@@ -40,7 +48,41 @@ namespace FlipManiaProgram5
             Console.WriteLine("Number of Heads: " + numberOfHeads);
             Console.WriteLine("Number of Tails: " + numberOfTails);
 
-            Console.WriteLine("We flipped a coin {0} times\nHeads: {1}\nTails: {2}", numFlip, numberOfHeads, numberOfTails);
+            //Console.WriteLine("We flipped a coin {0} times\nHeads: {1}\nTails: {2}", numFlip, numberOfHeads, numberOfTails);
+        }
+        /// <summary>
+        /// Function will flip a coin trying to find
+        /// a set number of heads, tracking number of
+        /// flips needed
+        /// </summary>
+        /// <param name="numberOfHeads">number of heads we want to find</param>
+        static void FlipForHeads(int numberOfHeads)
+        {
+            //set variables for number of heads found
+            //and total number of flips
+            int numberOfHeadsFlipped = 0;
+            int totalFlips = 0;
+            //new random number generator
+            Random rng = new Random();
+            
+            //loop setting conditions so it will stop
+            //when we reach  designated numbers
+            while (numberOfHeads != numberOfHeadsFlipped)
+            {
+                //setting variable to a new random number
+                int randomNumber = rng.Next(0, 2);
+
+                //conditions for flip
+                if (randomNumber == 0)
+                {
+                    //found heads, incrmenting variable
+                    numberOfHeadsFlipped++;
+                }
+                //incrementing variable regardless
+                totalFlips++;
+            }
+            //write outcome to console
+            Console.WriteLine("We are flipping a coin until we find {0} heads\nIt took {1} to find {2} heads.", numberOfHeads, totalFlips, numberOfHeads);
         }
     }
 }
---
+++ GroupMaker10000/GroupMaker10000/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupMaker10000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GroupMaker10000(4, 20);
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// makes a group maker that assigns people randomly to groups according
+        /// to group size and class size
+        /// </summary>
+        /// <param name="groupSize">how many people in each group</param>
+        /// <param name="classSize">how many people in each class</param>
+        static void GroupMaker10000(int groupSize, int classSize)
+        {
+            //new random number generate, and two lists to maniuplate groups
+            Random rng = new Random();
+            List<int> groupList = new List<int>();
+            List<int> classList = new List<int>();
+            
+            //populate class list
+            for (int i = 0; i < classSize; i++)
+            {
+                classList.Add(i + 1);
+                
+            }
+            //using random index, add students to group
+            //while removing them from list until list is empty
+            while (classList.Count() > 0)
+            {
+                int randomNum = rng.Next(0, classList.Count());
+                int aStudent = classList[randomNum];
+                groupList.Add(aStudent);
+                classList.Remove(aStudent);
+                
+                //if group list meets params, or classlist is empty,
+                //write to console the group and the elemnets in group
+                if(groupList.Count() == groupSize || classList.Count() == 0)
+                {
+                    Console.WriteLine("Group ");
+                    //write to console using foreach element in group
+                    foreach (int element in groupList)
+                    {
+                        Console.WriteLine(element);
+                    }
+                    //clear grouplist and begin loop again
+                    groupList.Clear();
+                }
+            }
+        }
+    }
+}
