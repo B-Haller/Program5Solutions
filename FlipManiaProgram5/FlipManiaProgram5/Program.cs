95861f2fb66b0c16886a91172e3605b9d0ec8b1e
SeedPaths-User <lessdismissive@email.com>
2014-10-29 18:07:08 -0600

    FlipMania
    Homework for Day 3

--- FlipManiaProgram5/FlipManiaProgram5/Program.cs
+++ FlipManiaProgram5/FlipManiaProgram5/Program.cs
@@ -22,12 +22,25 @@ namespace FlipManiaProgram5
             Random rng = new Random();
             int randomNumber = rng.Next(0, 2);
 
+            int numberOfHeads = 0;
+            int numberOfTails = 0;
+
             for (int i = 0; i <= numFlip; i++)
             {
                 randomNumber = rng.Next(0, 2);
-                Console.WriteLine();
+                if (randomNumber == 0)
+                {
+                    numberOfHeads++;
+                }
+                else
+                    numberOfTails++;
                 
             }
+            Console.WriteLine("We flipped a coin " + numFlip + " times.");
+            Console.WriteLine("Number of Heads: " + numberOfHeads);
+            Console.WriteLine("Number of Tails: " + numberOfTails);
+
+            Console.WriteLine("We flipped a coin {0} times\nHeads: {1}\nTails: {2}", numFlip, numberOfHeads, numberOfTails);
         }
     }
 }
