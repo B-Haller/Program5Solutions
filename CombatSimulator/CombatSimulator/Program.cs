6ff1598151e4467463796e29db0e63bafcee5fde
SeedPaths-User <lessdismissive@email.com>
2014-11-07 17:30:45 -0700

    CodeChallenge Week 2

---
+++ CombatSimulator/CombatSimulator/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombatSimulator
+{
+    class Program
+    {
+        static Random rng = new Random();
+        static int playerHP = 100;
+        static int computerHP = 200;
+        static string computerName = "His Supreme Undying Holy Kim Jung Ill, Touched By God";
+        static int sanctionsCount = 0;
+        static int tittyTwisterCount = 0;
+
+
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void DiplomacySimulator()
+        {
+
+        }
+
+
+        static void userAttack(int userInput)
+        {
+          int attackChance = rng.Next(0,101);
+
+          switch (userInput)
+          {
+              case 1:
+                  if (attackChance < 30)
+                  {
+                      Console.ForegroundColor = ConsoleColor.Red;
+                      Console.WriteLine("\n\nBad news. {0} has evaded your titty twister by slapping it away.", computerName);
+                      tittyTwisterCount++;
+                  }
+                  else if (30 <= attackChance && attackChance < 90)
+                  {
+                      Console.ForegroundColor = ConsoleColor.Green;
+                      int damageDelt = rng.Next(12, 20);
+                      Console.WriteLine("\n\nGood news, everyone. Your titty twister made {0}/ncry, and did {1} damage.", computerName, damageDelt);                      
+                      computerHP -= damageDelt;
+                      tittyTwisterCount++;
+                  }
+                  else if (90 <= attackChance && attackChance < 95)
+                  {
+                      Console.ForegroundColor = ConsoleColor.White;
+                      int healthRestore = rng.Next(5,25);
+                      Console.WriteLine("\n\nYou tweaked your own nipples. That feels nice. You been aroused and restored {0} HP.", healthRestore);
+                      playerHP += healthRestore;
+                      tittyTwisterCount++;
+                  }
+                  else
+                  {
+                      Console.ForegroundColor = ConsoleColor.Red;
+                      int healthRestore = rng.Next(5,25);
+                      Console.WriteLine("\n\nYou tweaked {0}'s nipples. He became aroused and it restored {1} of his HP.", computerName, healthRestore);
+                      computerHP += healthRestore;
+                      tittyTwisterCount++;
+                  }
+                  Console.WriteLine("\n\n\t\t\t<Press enter to plan your next move>");
+                  Console.ReadLine();
+                  Console.Clear();
+                  break;
+
+              case 2:
+
+                  Console.ForegroundColor = ConsoleColor.Green;
+                  int damageDeltSanctions = rng.Next(6,14);
+                  Console.WriteLine("\n\nYou sanctioned {0}, dealing {1} damage. Way to go Henry Kissinger.", computerName, damageDeltSanctions);
+                  Console.WriteLine("\n\n\t\t\t<Press enter to plan your next move>");
+                  Console.ReadLine();
+                  Console.Clear();
+                  break;
+
+              case 3:
+
+                  int healingDone = rng.Next(6,15);
+                  Console.ForegroundColor = ConsoleColor.Green;
+                  Console.WriteLine("You just farted in a diplomatic meeting.\nBut damn that felt good. HP increased by {0}.", healingDone);
+                  playerHP += healingDone;
+                  if (healingDone >= 13)
+                  {
+                      Console.ForegroundColor = ConsoleColor.Red;
+                      Console.WriteLine("Your fart, which built from a falafel, into the equivalent of nerve gas");
+                  }
+
+          }
+            }
+        }
+    }
+}
---
+++ Week2CodeChallenge/Week2CodeChallenge/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2CodeChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Title = "Week 2 Code Challenge";
+            Console.WriteLine("Fizzbuzz: except now with Fizz Buzz Crackle Pop.\n");
+            for (int i = 0; i < 20; i++)
+            {
+                FizzBuzz(i);
+            }
+
+            Console.WriteLine("\n\t\t\t<Hit enter to see #4: Letter Counter>");
+            Console.ReadLine();
+            Console.Clear();
+
+         
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            LetterCounter('i', "I love biscuits and gravy. It's the best breakfast ever.");
+            Console.WriteLine("\n");
+            LetterCounter('n', "Never gonna give you up. Never gonna let you down.");
+            Console.WriteLine("\n");
+            LetterCounter('s', "Sally sells seashells down by the seashore. She's from Sussex.");
+
+            Console.ReadLine();
+        }
+
+        static void FizzBuzz(int num)
+        {
+            if (num % 3 == 0 && num % 5 == 0)
+            {
+                Console.WriteLine("Fizz Buzz Crackel Pop");
+            }
+            else if (num % 5 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (num % 3 == 0)
+            {
+                Console.WriteLine("Buzzin'");
+            }
+            else
+            {
+                Console.WriteLine(num);
+            }
+        }
+
+        /// <summary>
+        /// counts letters by input and upper and lower
+        /// </summary>
+        /// <param name="letter">letter to be counted</param>
+        /// <param name="inString">string to check</param>
+        static void LetterCounter(char letter, string inString)
+        {
+            //set int equal to the count of uppercase letters in string
+            var upperCaseLetters = inString.Count(x => "ABCDEFGHIJKLMNIOPQRSTUVWYXZ".Contains(x.ToString()));
+            //set int equal to the count of lowercase letters in input string
+            var lowerCaseLetters = inString.Count(x => "abcdefigjklmniopqurstuvwyxz".Contains(x.ToString()));
+            //set int equal to the number of searched input letter in string (converting to lower)
+            var numberOfLetters = inString.ToLower().Count(x => x.ToString().Contains(letter.ToString()));
+            //writes the results to console
+            Console.WriteLine("Input: {0}\nNumber of Lowercases: {1}\nNumber of Uppercases: {2}\n" +
+                "Total number of letter {4} found: {3}", inString, lowerCaseLetters, upperCaseLetters, numberOfLetters, letter);
+           
+            
+        }
+    }
+}
